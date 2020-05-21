2054f400be4c4a2303ff9a33002e02d44033c908
Daryll Dimazana <darylldimazana22@gmail.com>
2020-05-21 23:48:50 +0800

    Update ganeshasPattern.js
    Pasted code to Visual Studio Code for consistent spacing

--- ganeshasPattern.js
+++ ganeshasPattern.js
@@ -1,80 +1,80 @@
 ganeshasPattern = n => {
-	// first line
-	let firstLine = ""
+    // first line
+    let firstLine = ""
 	
-  firstLine += "*";
-  
-  for (let i = 0; i < (n - 3) / 2; i++) {
-  	firstLine += " ";
-  }
-  
-  for (let i = 0; i < (n + 1) / 2; i++) {
-  	firstLine += "*";
-  }
-  
-  console.log(firstLine);
-  
-  // upper lines
-  for (let i = 0; i < (n - 3) / 2; i++) {
-  	let upperLine = "";
+    firstLine += "*";
     
-    upperLine += "*";
-    
-    for (let j = 0; j < (n - 3) / 2; j++) {
-    	upperLine += " ";
+    for (let i = 0; i < (n - 3) / 2; i++) {
+        firstLine += " ";
     }
     
-    upperLine += "*";
-    
-    for (let k = 0; k < ((n - 3) / 2) + 1; k++) {
-    	upperLine += " ";
+    for (let i = 0; i < (n + 1) / 2; i++) {
+        firstLine += "*";
     }
     
-    console.log(upperLine);
-  }
-  
-  // middle line
-  let middleLine = "";
-  
-  for (let i = 0; i < n; i++) {
-  	middleLine += "*";
-  }
-  
-  console.log(middleLine);
-  
-  // lower lines
-  for (let i = 0; i < (n - 3) / 2; i++) {
-  	let lowerLine = "";
+    console.log(firstLine);
     
-    for (let j = 0; j < ((n - 3) / 2) + 1; j++) {
-    	lowerLine += " ";
+    // upper lines
+    for (let i = 0; i < (n - 3) / 2; i++) {
+        let upperLine = "";
+        
+        upperLine += "*";
+        
+        for (let j = 0; j < (n - 3) / 2; j++) {
+            upperLine += " ";
+        }
+        
+        upperLine += "*";
+        
+        for (let k = 0; k < ((n - 3) / 2) + 1; k++) {
+            upperLine += " ";
+        }
+        
+        console.log(upperLine);
     }
     
-    lowerLine += "*";
+    // middle line
+    let middleLine = "";
     
-    for (let k = 0; k < (n - 3) / 2; k++) {
-    	lowerLine += " ";
+    for (let i = 0; i < n; i++) {
+        middleLine += "*";
     }
     
-   	lowerLine += "*";
+    console.log(middleLine);
     
-    console.log(lowerLine);
-  }
-  
-  // last line
-  let lastLine = "";
-   
-  for (let i = 0; i < (n + 1) / 2; i++) {
-  	lastLine += "*";
-  }
-  
-  for (let i = 0; i < (n - 3) / 2; i++) {
-  	lastLine += " ";
-  }
+    // lower lines
+    for (let i = 0; i < (n - 3) / 2; i++) {
+        let lowerLine = "";
+        
+        for (let j = 0; j < ((n - 3) / 2) + 1; j++) {
+            lowerLine += " ";
+        }
+        
+        lowerLine += "*";
+        
+        for (let k = 0; k < (n - 3) / 2; k++) {
+            lowerLine += " ";
+        }
+        
+        lowerLine += "*";
+        
+        console.log(lowerLine);
+    }
+    
+    // last line
+    let lastLine = "";
+    
+    for (let i = 0; i < (n + 1) / 2; i++) {
+        lastLine += "*";
+    }
+    
+    for (let i = 0; i < (n - 3) / 2; i++) {
+        lastLine += " ";
+    }
 
-  lastLine += "*";
-  
-  console.log(lastLine);
+    lastLine += "*";
+    
+    console.log(lastLine);
 }
 
 ganeshasPattern(11);
