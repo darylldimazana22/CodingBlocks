db080d5e922be4e8f79e6df77027a141dedf45a0
Daryll Dimazana <darylldimazana22@gmail.com>
2020-05-31 00:28:44 +0800

    Create magical_park.js

---
+++ magical_park.js
@@ -0,0 +1,40 @@
+magical_park = (a, m, n, k, s) => {
+    let success = true;
+  
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            let ch = a[i][j];
+            
+            if (s < k) {
+                success = false;
+                break;
+            }
+            
+            if (ch == '*') {
+                s += 5;
+            } else if (ch == '.') {
+                s -= 2;
+            } else {
+                break;
+            }
+            
+            if (j != n - 1) {
+            s--;
+            }
+        }
+    }
+
+    if (success) {
+        console.log("Yes");
+        console.log(s);
+    } else {
+        console.log("No");
+    }
+}
+
+magical_park([
+    [".", ".", "*", "."], 
+    [".", "#", ".", "."],
+    ["*", "*", ".", "."],
+    [".", "#", "*", "*"]],
+    4, 4, 5, 20);
